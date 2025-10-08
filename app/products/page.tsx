4c0fe38cad682a8723add368c34864ab83eefa9c
ahsown <ahsown3274@gmail.com>
2025-10-08 19:56:54 +0800

    - 新增对设备产品的分页展示功能

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -2,45 +2,11 @@
 import Image from "next/image";
 import { Metadata } from "next";
 import { ContactSection } from "../components/ContactSection";
+import HomeContent from "./HomeContent";
 
 export const metadata: Metadata = {
   title: "产品展示",
 };
-
-function ProductsList() {
-  const path = "/products"
-  const products = [
-    { title: "15吨产品货运索道", image: "/product1.png", describe: "安全可靠的大型索道架设", feature: "高效、安全、可靠" },
-    { title: "5吨小型索道", image: "/product4.jpg", describe: "适用范围较广的中型索道架设", feature: "高效、安全、可靠" },
-    { title: "1.5吨循环式索道", image: "/product5.jpg", describe: "适用于小型项目的使用", feature: "灵活、便捷、安全" },
-    { title: "地锚过渡筒", image: "/product2.jpg", describe: "", feature: "" },
-    { title: "10吨用卷扬机", image: "/product3.jpg", describe: "", feature: "" },
-    { title: "循环式卷扬机", image: "/product6.jpg", describe: "", feature: "" },
-  ]
-  function ProductCard({ title, image, describe, feature }: { title: string, image: string, describe: string, feature: string }) {
-    return (
-      < div className="bg-white shadow-md p-4" >
-        <div className=" w-[150px] h-[150px] md:w-[300px] md:h-[300px]">
-          <Image src={path + image} alt={title} width={300} height={300} className=" rounded-lg object-cover w-full h-full" />
-        </div>
-        {/* <div className="bg-[url(/product1.png)] bg-cover w-[300px] h-[200px]"></div> */}
-        <h3 className="text-xl font-bold mt-2">{title}</h3>
-        {describe === "" ? null : <p className="text-lg mb-2">{describe}</p>}
-        {feature === "" ? null : <p className="text-gray-600">主要特点：{feature}</p>}
-      </div >
-    )
-  }
-  return (
-    <section id="product-list" className="mb-8">
-      
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {products.map((product) => (
-          <ProductCard key={product.title} {...product} />
-        ))}
-      </div>
-    </section>
-  );
-}
 
 export default function Products() {
   return (
@@ -53,8 +19,9 @@ export default function Products() {
             公司提供多种高性能的索道和缆车产品，满足不同客户的需求。
           </p>
         </section>
-
-        <ProductsList />
+        
+        {/* <ProductsList /> */}
+        <HomeContent />
 
         {/* 联系方式 */}
         <ContactSection />
